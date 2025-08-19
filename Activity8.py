88c78fce7f685495d87bb2a8d0f28f48b94bd27e
JadeAngelique <pilarcajadeangeliquez@gmail.com>
2025-08-19 20:51:36 +0800

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,8 @@
+balance = 1600
+withdrawal = 300
+b = 2
+a = 2
+name1 = 'Jade'
+name2 = 'jade'
+#print(b >= a )
+print(name1 != name2)
---
+++ Activity9.py
@@ -0,0 +1,7 @@
+username = 'Jade'
+password = '101255'
+
+#print(username == 'Jade')
+print((username == 'Jade')and(password == '101255'))
+print((username == 'Jade')or(password == '101255'))
+print(not((username == 'Jade')or(password == '101255')))
