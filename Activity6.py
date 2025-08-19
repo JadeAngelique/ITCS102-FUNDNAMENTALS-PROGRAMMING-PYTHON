63e012cfee0bead1613e6e5a06ee426e0eb160f5
JadeAngelique <pilarcajadeangeliquez@gmail.com>
2025-08-19 20:30:13 +0800

    Activity6.py

--- Activity6.py
+++ Activity6.py
@@ -1,5 +1,17 @@
-something =eval(input("Input something -->"))
-print("The data type of something is",type(something))
+n1 =eval(input("Enter the first number : "))
+n2 =eval(input("Enter the second number : "))
 
-answer = 7 + something
-print("The answer is", answer)
+s = n1 + n2
+d = n1 - n2
+p = n1 * n2
+q = n1 / n2
+
+
+#solution = ((n1 / n2) * 100 - 5 ) // 300
+print("\nThe sum of",n1,"and",n2,"is",s)
+print("The difference of",n1,"and",n2,"is",d)
+print("The product of",n1,"and",n2,"is",p)
+print("The quotient of",n1,"and",n2,"is",q)
+print(n1, "exponent by",n2,"is",n1**n2)
+print("The remainder of",n1,"and",n2,"is",n1 % n2)
+print("The floor division of",n1,"and",n2,"is",n1//n2)
