3d8944c792c3b82ff7f72ac11290312397f98928
JadeAngelique <pilarcajadeangeliquez@gmail.com>
2025-08-19 20:50:57 +0800

    Activity7.py

--- Activity7.py
+++ Activity7.py
@@ -1,17 +1,12 @@
-n1 =eval(input("Enter the first number : "))
-n2 =eval(input("Enter the second number : "))
+a = 5
+print("the value of a is ", a)
 
-s = n1 + n2
-d = n1 - n2
-p = n1 * n2
-q = n1 / n2
+a += 5
+print("the value of a is ", a)
 
-
-#solution = ((n1 / n2) * 100 - 5 ) // 300
-print("\nThe sum of",n1,"and",n2,"is",s)
-print("The difference of",n1,"and",n2,"is",d)
-print("The product of",n1,"and",n2,"is",p)
-print("The quotient of",n1,"and",n2,"is",q)
-print(n1, "exponent by",n2,"is",n1**n2)
-print("The remainder of",n1,"and",n2,"is",n1 % n2)
-print("The floor division of",n1,"and",n2,"is",n1//n2)
+a = a + 5
+a += 7 
+a += 12
+a *= 9 
+a -= 5
+print("the value of a is ", a)
