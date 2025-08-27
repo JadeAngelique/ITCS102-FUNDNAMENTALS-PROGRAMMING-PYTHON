885378ccda323011935965aece5a486cdc861453
JadeAngelique <pilarcajadeangeliquez@gmail.com>
2025-08-27 11:12:28 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = 'Jade Angelique'
+pwd = "110706"
+
+username = input ('Enter username: ')
+password = input ('Enter password: ')
+
+if username == "Jade Angelique" and password == "110706":
+	print("Acess Granted")
+else: 	
+	print("Access Denied")
