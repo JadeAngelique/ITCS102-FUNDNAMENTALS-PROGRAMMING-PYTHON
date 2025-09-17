6cf6d602ecaab65be27a2cfb1c99201df748ba69
JadeAngelique <pilarcajadeangeliquez@gmail.com>
2025-09-17 20:30:02 +0800

    Add files via upload

---
+++ Codde_Challenge9.py
@@ -0,0 +1,10 @@
+print("COUNTDOWN TIMER SIMULATOR")
+
+start = eval(input("Enter the starting number for countdown: "))
+
+print("Countdown started:")
+
+for i in range(start, 0, -1):
+    print(i)
+
+print("Liftoff!")
